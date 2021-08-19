19715e4aaa7894ef8b785da8920a019a499419e3
liuyangaq <liuyangaq@yonyou.com>
2021-08-19 23:15:58 +0800

    添加useMount和useDebounce两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -14,22 +14,40 @@ export const ProjectListScreen = () => {
         name: '',
         personId: ''
     })
+    const debounceParam = useDebounce(param, 2000)
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response => {
             if (response.ok) {
                 setList(await response.json())
             }
         })
-    }, [param])
-    useEffect(() => {
+    }, [debounceParam])
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async response => {
             if (response.ok) {
                 setUsers(await response.json())
             }
         })
-    }, [])
+    })
     return <div>
         <SearchPanel users={users || []} param={param} setParam={setParam}></SearchPanel>
         <List users={users} list={list}></List>
     </div>
+}
+
+export const useMount = (callback) => {
+    useEffect(() => {
+    callback()
+    }, [])
+}
+
+export const useDebounce = (value, delay) => {
+    const [debouncedValue, setdebouncedValue] = useState(value)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setdebouncedValue(value), delay)
+        // 每次在上一次useEffect处理完以后再运行
+        return () => clearTimeout(timeout)
+    }, [value, delay])
+    return debouncedValue;
 }
