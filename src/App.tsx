cbf4dfbf0d9787d659593bfd080fcec79e417817
liuyangaq <liuyangaq@yonyou.com>
2021-10-29 15:29:28 +0800

    添加error-boundray

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { AuthenticateedApp } from 'authenticated-app';
-import { useAuth } from 'context/auth-context';
-import React from 'react';
-import { UnauthenticatedApp } from 'unauthenticated-app';
-import './App.css';
+import { AuthenticateedApp } from "authenticated-app";
+import { ErrorBoundary } from "components/error-boundary";
+import { FullPageErrorFallback } from "components/lib";
+import { useAuth } from "context/auth-context";
+import React from "react";
+import { UnauthenticatedApp } from "unauthenticated-app";
+import "./App.css";
 
 function App() {
-  const { user } = useAuth()
+  const { user } = useAuth();
   return (
     <div className="App">
-      {user? <AuthenticateedApp/> : <UnauthenticatedApp/>}
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticateedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
